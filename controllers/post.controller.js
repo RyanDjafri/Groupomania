78f8c155a80bdaf7ec929b530c07a17329ca3d52
RyanDjafri <ryandjafri@gmail.com>
2022-10-18 10:33:24 +0200

    suite back end groupomania debut post

---
+++ controllers/post.controller.js
@@ -0,0 +1,27 @@
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+module.exports.readPost = (req, res) => {
+  PostModel.find((err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("Error to get data :" + err);
+  });
+};
+module.exports.createPost = (req, res) => {
+  const newPost = new postModel({
+    posterId: req.body.posterId,
+    message: req.body.message,
+    video: req.body.video,
+    likers: [],
+    comments: [],
+  });
+  try {
+      const post = await newPost.save();
+      return res.status(201).json(post);
+  }catch {
+      return res.status(400).send(err);
+  }
+};
+module.exports.updatePost = (req, res) => {};
+module.exports.deletePost = (req, res) => {};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,8 @@ const userController = require("../controllers/user.controller");
 
 //auth
 router.post("/register", authController.signUp);
-
+router.post("/login", authController.signIn);
+router.get("/logout", authController.logOut);
 //user DB
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
