d0d4a9d58715af0c343578c6d8e89d437dbaf3d0
RyanDjafri <ryandjafri@gmail.com>
2022-10-19 11:05:36 +0200

    début post comments

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,15 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
 //auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
 router.get("/logout", authController.logOut);
+
 //user DB
 router.get("/", userController.getAllUsers);
 router.get("/:id", userController.userInfo);
@@ -14,4 +18,6 @@ router.delete("/:id", userController.deleteUser);
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unFollow);
 
+//upload
+router.post("/upload", upload.single("file"), uploadController.uploadProfil);
 module.exports = router;
