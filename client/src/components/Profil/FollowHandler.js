417ed2a536e922aa52744052538f187ef8b44f41
RyanDjafri <ryandjafri@gmail.com>
2022-11-04 10:16:09 +0100

    debut page home, trending, affichage postss

--- client/src/components/Profil/FollowHandler.js
+++ client/src/components/Profil/FollowHandler.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { isEmpty } from "../Utils";
+import { userReducer } from "../../reducers/user.reducer";
+import { followUser, unfollowUser } from "../../actions/user.actions";
 
-const FollowHandler = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const FollowHandler = (idToFollow) => {
+  const userData = useSelector((state) => state.userReducer);
+  const [isFollowed, setIsFollowed] = useState(false);
+  const handleFollow = () => {
+    dispatch(followUser(userData._id, idToFollow));
+    setIsFollowed(true);
+  };
+  const handleUnfollow = () => {
+    dispatch(unfollowUser(userData._id, idToFollow));
+    setIsFollowed(false);
+  };
+  useEffect(() => {
+    if (!isEmpty(userData.following)) {
+      if (userData.following.includes(idToFollow)) {
+        setIsFollowed(true);
+      } else setIsFollowed(false);
+    }
+  }, [userData, idToFollow]);
+  return (
+    <>
+      {isFollowed && isEmpty(userData) && (
+        <span onClick={handleUnfollow}>
+          <button className="unfollow-btn">Abonné</button>
+        </span>
+      )}
+      {isFollowed === false && !isEmpty(userData) && (
+        <span onClick={handleFollow}>
+          <button className="follow-btn">Suivre</button>
+        </span>
+      )}
+    </>
+  );
 };
 
 export default FollowHandler;
