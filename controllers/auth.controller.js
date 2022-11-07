79ede9af4e55443718e70a1b29a3f80c7ab0c266
RyanDjafri <ryandjafri@gmail.com>
2022-11-07 15:16:37 +0100

    correction erreur back end et probleme token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,9 @@
 const UserModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const { signUpErrors, signInErrors } = require("../utils/errors.utils");
-const maxAge = 3 * 21 * 60 * 60 * 1000;
+
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_SECRET, {
     expiresIn: maxAge,
@@ -13,7 +15,7 @@ module.exports.signUp = async (req, res) => {
 
   try {
     const user = await UserModel.create({ pseudo, email, password });
-    res.satus(201).json({ user: user._id });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     const errors = signUpErrors(err);
     res.status(200).send({ errors });
@@ -26,7 +28,7 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge });
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
     res.status(200).json({ user: user._id });
   } catch (err) {
     const errors = signInErrors(err);
@@ -34,7 +36,7 @@ module.exports.signIn = async (req, res) => {
   }
 };
 
-module.exports.logOut = (req, res) => {
+module.exports.logout = (req, res) => {
   res.cookie("jwt", "", { maxAge: 1 });
   res.redirect("/");
 };
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const userRoutes = require("./routes/user.routes");
-const postRoutes = require("./routes/post.routes");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const userRoutes = require("./routes/user.routes");
+const postRoutes = require("./routes/post.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const cors = require("cors");
+
 const app = express();
 
 const corsOptions = {
@@ -17,23 +18,23 @@ const corsOptions = {
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
 };
-//middleware
 app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//jwt
+// jwt
 app.get("*", checkUser);
 app.get("/jwtid", requireAuth, (req, res) => {
   res.status(200).send(res.locals.user._id);
 });
 
-//routes
+// routes
 app.use("/api/user", userRoutes);
-app.use("/api.post", postRoutes);
+app.use("/api/post", postRoutes);
 
-//server
+// server
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
