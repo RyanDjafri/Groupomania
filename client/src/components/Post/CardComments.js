4fbc52253927712a002e75bfd5afceb4192e5da4
RyanDjafri <ryandjafri@gmail.com>
2022-11-07 15:50:11 +0100

    debut gestion commentaires des posts

--- client/src/components/Post/CardComments.js
+++ client/src/components/Post/CardComments.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import FollowHandler from "../Profil/FollowHandler";
+import { isEmpty, timestampParser } from "../Utils";
 
-const CardComments = () => {
-  return <div></div>;
+const CardComments = ({ post }) => {
+  const [text, setText] = useState("");
+  const usersData = useSelector((state) => state.usersReducer);
+  const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+  const handleComment = () => {};
+  return (
+    <div className="comments-container">
+      {post.comments.map((comment) => {
+        return (
+          <div>
+            <div
+              className={
+                commment.commenterId === userData._id
+                  ? "comment-container client"
+                  : "comment-container"
+              }
+              key={comment._id}>
+              <div className="left-part">
+                <img
+                  src={
+                    !isEmpty(usersData[0]) &&
+                    usersData
+                      .map((user) => {
+                        if (user._id === post.comment.commenterId)
+                          return user.picture;
+                        else return null;
+                      })
+                      .join("")
+                  }
+                  alt="commenter-pic"
+                />
+              </div>
+              <div className="right-part">
+                <div className="comment-header">
+                  <div className="pseudo">
+                    <h3>{comment.commenterPseudo}</h3>
+                    {comment.commenterId !== userData._id && (
+                      <FollowHandler
+                        idToFollow={comment.commenterId}
+                        type={"card"}
+                      />
+                    )}
+                  </div>
+                  <span>{timestampParser(comment.timestamp)}</span>
+                </div>
+                <p>{comment.text}</p>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      {userData._id && (
+        <form action="" onSubmit={handleComment} className="comment-form">
+          <input
+            type="text"
+            name="text"
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+            placeholder="Laisser un commentaire"
+          />
+          <br />
+          <input type="submit" value={Envoyer} />
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default CardComments;
