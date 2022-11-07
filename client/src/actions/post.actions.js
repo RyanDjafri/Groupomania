5fe72325a099cc4435a8b43e721af3665c1e91b6
RyanDjafri <ryandjafri@gmail.com>
2022-11-07 15:56:46 +0100

    correction quelques erreurs

--- client/src/actions/post.actions.js
+++ client/src/actions/post.actions.js
@@ -3,6 +3,7 @@ import axios from "axios";
 //posts
 
 export const GET_POSTS = "GET_POSTS";
+export const GET_ALL_POSTS = "GET_ALL_POSTS";
 export const LIKE_POST = "LIKE_POST";
 export const UNLIKE_POST = "UNLIKE_POST";
 export const UPDATE_POST = "UPDATE_POST";
@@ -14,10 +15,10 @@ export const getPosts = (num) => {
       .get(`${process.env.REACT_APP_API_URL}api/post/`)
       .then((res) => {
         const array = res.data.slice(0, num);
-        dispatch({ type: GET_POSTS, payload: array }).catch((err) =>
-          console.log(err)
-        );
-      });
+        dispatch({ type: GET_POSTS, payload: array });
+        dispatch({ type: GET_ALL_POSTS, payload: res.data });
+      })
+      .catch((err) => console.log(err));
   };
 };
 
