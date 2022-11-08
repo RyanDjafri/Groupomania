17fd3002dc659302b1f6571dc72e785740bd370f
RyanDjafri <ryandjafri@gmail.com>
2022-11-08 13:29:34 +0100

    correction token, page home

--- client/src/components/Post/CardComments.js
+++ client/src/components/Post/CardComments.js
@@ -10,57 +10,57 @@ const CardComments = ({ post }) => {
   const usersData = useSelector((state) => state.usersReducer);
   const userData = useSelector((state) => state.userReducer);
   const dispatch = useDispatch();
+
   const handleComment = (e) => {
     e.preventDefault();
+
     if (text) {
       dispatch(addComment(post._id, userData._id, text, userData.pseudo))
         .then(() => dispatch(getPosts()))
         .then(() => setText(""));
     }
   };
+
   return (
     <div className="comments-container">
       {post.comments.map((comment) => {
         return (
-          <div>
-            <div
-              className={
-                commment.commenterId === userData._id
-                  ? "comment-container client"
-                  : "comment-container"
-              }
-              key={comment._id}>
-              <div className="left-part">
-                <img
-                  src={
-                    !isEmpty(usersData[0]) &&
-                    usersData
-                      .map((user) => {
-                        if (user._id === post.comment.commenterId)
-                          return user.picture;
-                        else return null;
-                      })
-                      .join("")
-                  }
-                  alt="commenter-pic"
-                />
-              </div>
-              <div className="right-part">
-                <div className="comment-header">
-                  <div className="pseudo">
-                    <h3>{comment.commenterPseudo}</h3>
-                    {comment.commenterId !== userData._id && (
-                      <FollowHandler
-                        idToFollow={comment.commenterId}
-                        type={"card"}
-                      />
-                    )}
-                  </div>
-                  <span>{timestampParser(comment.timestamp)}</span>
+          <div
+            className={
+              comment.commenterId === userData._id
+                ? "comment-container client"
+                : "comment-container"
+            }
+            key={comment._id}>
+            <div className="left-part">
+              <img
+                src={
+                  !isEmpty(usersData[0]) &&
+                  usersData
+                    .map((user) => {
+                      if (user._id === comment.commenterId) return user.picture;
+                      else return null;
+                    })
+                    .join("")
+                }
+                alt="commenter-pic"
+              />
+            </div>
+            <div className="right-part">
+              <div className="comment-header">
+                <div className="pseudo">
+                  <h3>{comment.commenterPseudo}</h3>
+                  {comment.commenterId !== userData._id && (
+                    <FollowHandler
+                      idToFollow={comment.commenterId}
+                      type={"card"}
+                    />
+                  )}
                 </div>
-                <p>{comment.text}</p>
-                <EditDeleteComment comment={comment} postId={post._id} />
+                <span>{timestampParser(comment.timestamp)}</span>
               </div>
+              <p>{comment.text}</p>
+              <EditDeleteComment comment={comment} postId={post._id} />
             </div>
           </div>
         );
@@ -75,7 +75,7 @@ const CardComments = ({ post }) => {
             placeholder="Laisser un commentaire"
           />
           <br />
-          <input type="submit" value={Envoyer} />
+          <input type="submit" value="Envoyer" />
         </form>
       )}
     </div>
